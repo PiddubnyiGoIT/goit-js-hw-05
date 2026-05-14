dec2229590e914945a93d7cf2cab32290ae62e1b
Oleksandr Piddubnyi <mikssasha2016@gmail.com>
2026-05-14 21:40:00 +0200

    2

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,7 @@
 const sortByDescendingFriendCount = (users) => {
-  return [...users].sort((a, b) => b.friends.length - a.friends.length);
+  return users.toSorted(
+    (a, b) => b.friends.length - a.friends.length
+  );
 };
 
 console.log(
@@ -78,3 +80,4 @@ console.log(
 //     gender: "male"
 //   }
 // ]
+
